2c43521695e58941fe6afb69f3ab944be4fc0e10
Pankaj Bagul <pankaj.bagul@hmrc.gis.gov.uk>
2016-11-07 14:05:09 +0000

    Added Test

---
+++ src/test/java/com/howtodoinjava/jersey/HelloWorldImplTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.howtodoinjava.jersey;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author pankajbagul
+ *
+ */
+public class HelloWorldImplTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
